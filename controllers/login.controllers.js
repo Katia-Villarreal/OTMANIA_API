486c41ecbc8d8b87f7b8e2d7c72b5e9b8a1e7ee7
Katia-Villarreal <a00842228@tec.mx>
2026-04-17 01:00:46 -0600

    Cambio en el Login

--- controllers/login.controllers.js
+++ controllers/login.controllers.js
@@ -1,4 +1,5 @@
 import { db_connect } from "../Utils/db.js";
+import { hash } from "../Utils/hash.js";
 
 export const login = async (req, res) => {
     const sql = db_connect();
@@ -6,7 +7,7 @@ export const login = async (req, res) => {
     const text = "SELECT * FROM Users WHERE Email = $1";
     const values = [Email];
     const result = await sql.query(text, values);
-    const salt = result.rows[0].Password.substring(0, process.env.SALT_SIZE);
+    const salt = result.rows[0].password.substring(0, process.env.SALT_SIZE);
     const hashed = hash(Password, salt);
     console.log(result.rows[0]);
 }
